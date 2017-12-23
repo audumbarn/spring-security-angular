9b268d63c9cbed85254e3136cfafd5bb5f3af691
Audumbar Nevarekar <audumbar.raj.nevarekar@dxc.com>
2017-12-23 23:39:11 +0530

    Initial commit

---
+++ src/main/java/com/projects/ssa/SpringSecurityAngularApplication.java
@@ -0,0 +1,12 @@
+package com.projects.ssa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAngularApplication.class, args);
+	}
+}
