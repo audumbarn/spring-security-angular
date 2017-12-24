aeb0d3132715f5d3c39f1961a98bab504f1362b1
Audumbar Nevarekar <audumbar.raj.nevarekar@dxc.com>
2017-12-24 12:40:40 +0530

    Removed rest annotations

--- src/main/java/com/projects/ssa/SpringSecurityAngularApplication.java
+++ src/main/java/com/projects/ssa/SpringSecurityAngularApplication.java
@@ -1,9 +1,5 @@
 package com.projects.ssa;
 
-import java.security.Principal;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,8 +9,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 @SpringBootApplication
 public class SpringSecurityAngularApplication {
 
@@ -38,18 +32,5 @@ public class SpringSecurityAngularApplication {
 	    }
 	  }
 	 
-	 @RequestMapping("/resource")
-		public Map<String,Object> home() {
-			Map<String, Object> model = new HashMap<String, Object>();
-			model.put("id", UUID.randomUUID().toString());
-			model.put("content", "Hello World");
-			
-			return model;
-			
-		}
-		
-		@RequestMapping("/user")
-		  public Principal user(Principal user) {
-		    return user;
-		  }
+	 
 }
